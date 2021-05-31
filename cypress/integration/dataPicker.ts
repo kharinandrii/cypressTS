ce477dab6178a91613a60a5b5c02a7f61d8e1926
wicker_man <andrej.kharin@gmail.com>
2021-05-31 06:46:11 +0100

    add datapicker and assert

--- cypress/integration/dataPicker.ts
+++ cypress/integration/dataPicker.ts
@@ -1,24 +1,47 @@
 ///<reference types="Cypress" />
 describe("Tests block one", () => {
     it("datapicker test", () => {
-        cy.visit("https://demoqa.com/date-picker");
 
-        let date = new Date();
-        console.log(date.setDate(date.getDate() + 7));
-        let futureDay = date.getDay();
-        let futureMonth = date.toLocaleString('default', {month: '2-digit'});
-        console.log(futureDay)
-        console.log(futureMonth)
-        cy.get('#datePickerMonthYearInput').invoke('attr', 'value').then( dateInputAttr =>{
-            cy.get('#datePickerMonthYearInput').click()
+
+        function selectDayFromCurrent(){
+            let date = new Date();
+            date.setDate(date.getDate() + 29);
+             let futureDay = date.getDate();
+             console.log(futureDay)
+             let futureMonth = date.toLocaleString('default', {month: 'long'});
+             let assertDate:string;
+             assertDate =  date.toLocaleString('default', {month: '2-digit'}) + '/' + date.toLocaleString('default', {day: '2-digit'}) + '/' + date.getFullYear();
+            cy.get('.react-datepicker__current-month').invoke('text').then( dateInputAttr =>{
+                
             if(!dateInputAttr?.includes(futureMonth)){
                 cy.get('[aria-label="Next Month"]').click();
-                cy.get('.react-datepicker__month').contains(futureDay).click();
+                selectDayFromCurrent()
             }else{
-                cy.get('.react-datepicker__month').contains(futureDay).click();
-            }
-        })
+                cy.get(`[class=\'react-datepicker__day react-datepicker__day--0${date.toLocaleString('default', {day: '2-digit'})}\']`)
+                .then(listing => {
+                    const listingCount = Cypress.$(listing).length;
+                    console.log(listingCount)
+                    if(listingCount>2){
+                        cy.wrap(listing).eq(2).click();
+                    }else{
+                        cy.wrap(listing).click({ multiple: true });
+                    }
+                })
+        }
         
-//продолжить с видео по датапикер 
     })
+    return assertDate;
+}
+
+        cy.visit("https://demoqa.com/date-picker");
+
+       
+        cy.get('#datePickerMonthYearInput').click()
+       let assertDate = selectDayFromCurrent();
+        
+        cy.get('#datePickerMonthYearInput').invoke('attr', 'value').should('contain', assertDate);
+//продолжить с видео по датапикер 
+    
+    
+})
 })
